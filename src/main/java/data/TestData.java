4a8ce75d63c2d3dd759cf72f2f3b5a9f65b4786d
dav <davidvardanyan94@gmail.com>
2022-02-22 10:17:05 +0400

    changed main url

--- src/main/java/data/TestData.java
+++ src/main/java/data/TestData.java
@@ -1,7 +1,7 @@
 package data;
 
 public class TestData {
-    public static final String BASE_URL = "http://spv2-stage.betcoapps.com/";
+    public static final String BASE_URL = "localhost:3000";
     public static final String USER_EMAIL = "davit.vardanyan@betconstruct.com";
     public static final String USER_PASSWORD = "Test1235";
     public static final String USER_CURRENCY_AMD = "AMD";
